43d5c439c76e743c08579cc3eed9975edd24a69a
최승준 <tmdwns63@gmail.com>
2024-08-06 16:46:39 +0900

    Chore : 깃이그노어 수정
    폭파직전

---
+++ DoTeacher/app/src/main/java/com/example/doteacher/ui/preference/PreferenceFragment.kt
@@ -0,0 +1,19 @@
+package com.example.doteacher.ui.preference
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.doteacher.R
+import com.example.doteacher.databinding.FragmentPreferenceBinding
+import com.example.doteacher.ui.base.BaseFragment
+
+
+class PreferenceFragment : BaseFragment<FragmentPreferenceBinding>(R.layout.fragment_preference) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+
+}
