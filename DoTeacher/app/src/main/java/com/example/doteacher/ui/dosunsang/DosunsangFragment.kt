bf2988bef38ddf43e3afb5928c7aaf0c4c2f8ca9
최승준 <tmdwns63@gmail.com>
2024-08-12 16:41:56 +0900

    UI : design
    ui design

--- DoTeacher/app/src/main/java/com/example/doteacher/ui/dosunsang/DosunsangFragment.kt
+++ DoTeacher/app/src/main/java/com/example/doteacher/ui/dosunsang/DosunsangFragment.kt
@@ -1,5 +1,6 @@
 package com.example.doteacher.ui.dosunsang
 
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.doteacher.R
 import com.example.doteacher.data.model.param.UserParam
@@ -14,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class DosunsangFragment : BaseFragment<FragmentDosunsangBinding>(R.layout.fragment_dosunsang){
 
     private val dosunsangViewModel: DosunsangViewModel by viewModels()
+    private var isRobotActive = false
 
     override fun initView(){
         initData()
@@ -23,11 +25,29 @@ class DosunsangFragment : BaseFragment<FragmentDosunsangBinding>(R.layout.fragme
     override fun onResume() {
         super.onResume()
         initData()
-
     }
 
     private fun clickEventListener(){
         binding.connect.setOnClickListener {
+            toggleRobotState()
+        }
+    }
+
+    private fun toggleRobotState() {
+        if (isRobotActive) {
+            // 로봇이 활성화 상태일 때 슬립 모드로 전환
+            binding.robotlottie.pauseAnimation()
+            binding.robotlottie.visibility = View.GONE
+            binding.sleeplottie.visibility = View.VISIBLE
+            binding.sleeplottie.playAnimation()
+        } else {
+            // 로봇이 슬립 상태일 때 활성화 모드로 전환
+            binding.sleeplottie.pauseAnimation()
+            binding.sleeplottie.visibility = View.GONE
+            binding.robotlottie.visibility = View.VISIBLE
+            binding.robotlottie.playAnimation()
+
+            // 여기에 recommend 함수 호출
             val userParam = UserParam(
                 userEmail = SingletonUtil.user!!.userEmail,
                 userName = SingletonUtil.user!!.userName,
@@ -37,14 +57,18 @@ class DosunsangFragment : BaseFragment<FragmentDosunsangBinding>(R.layout.fragme
                 userTuto = true,
                 prefSelect = false
             )
-
             dosunsangViewModel.recommend(userParam)
         }
+
+        isRobotActive = !isRobotActive
     }
 
     private fun initData(){
         binding.userData = SingletonUtil.user
-        binding.robotlottie.playAnimation()
+        binding.sleeplottie.visibility = View.VISIBLE
+        binding.robotlottie.visibility = View.GONE
+        binding.sleeplottie.playAnimation()
+        binding.marquee.isSelected = true
+        binding.marquee.requestFocus()
     }
 }
-
